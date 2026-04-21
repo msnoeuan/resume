f547cfde9ef71e5b0d8a0ac6f91776972ed1f48e
Seungmin Yoo <msnoeuan@gmail.com>
2026-04-22 01:22:27 +0900

    fix: simplify next config for images

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || 'resume';
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: basePath,
-  assetPrefix: assetPrefix,
+  basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
   images: {
     unoptimized: true,
   },
